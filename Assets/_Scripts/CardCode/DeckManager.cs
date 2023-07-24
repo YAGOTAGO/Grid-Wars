f1a77c47e1d06f6da8889582da41384317a36a1b
YAGOTAGO <tiagocaetanodavies@gmail.com>
2023-07-24 15:06:38 -0400

    Coroutine list in Deck Manager added

--- Assets/_Scripts/CardCode/DeckManager.cs
+++ Assets/_Scripts/CardCode/DeckManager.cs
@@ -1,13 +1,20 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
     public static DeckManager Instance;
 
+    [Header("Deck Nums")]
+    [SerializeField] private TextMeshProUGUI _deckNumTMP;
+    [SerializeField] private TextMeshProUGUI _discardNumTMP;
+
+    [Header("Prefabs")]
     [SerializeField] private GameObject _cardTemplate;
 
     [Header("Location Transforms")]
@@ -18,15 +25,20 @@ public class DeckManager : MonoBehaviour
     [SerializeField] private Transform _canvas;
 
     [Header("Tween Values")]
-    [SerializeField, Range(0,20)] private float _duration = 1f;
-    [SerializeField, Range(0,2)] private float _drawDelay = 1f;
+    [SerializeField, Range(0,20)] private float _tweenDuration;
+    [SerializeField, Range(0,2)] private float _actionsDelay;
     [SerializeField] private Ease _ease;
 
-    #region Groups of cards
+    #region lists of cards
     public List<AbstractCard> _deck = new();
     public List<AbstractCard> _discard = new();
     public List<AbstractCard> _hand = new();
     private Dictionary<GameObject, int> _cardPrefabsInHand = new();
+    #endregion
+
+    #region Coroutine Queue
+    private Queue<Func<IEnumerator>> methodQueue = new();
+    private bool isQueueRunning = false;
     #endregion
 
     private void Awake()
@@ -36,16 +48,19 @@ public class DeckManager : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HandCardToDiscard(_cardPrefabsInHand.Keys.First());
+        }
     }
     private void Start()
     {
         AddToDeck(_deck, new TestCard());
-        AddToDeck(_deck, new TestCard());
-        AddToDeck(_deck, new TestCard());
-        ShuffleDeck();
-        DeckDraw(2);
-        
+        DeckDraw(1);
+        _deck.Add(new TestCard());
+        _deck.Add(new TestCard());
+        ShuffleList(_deck);
+         
     }
 
     /// <summary>
@@ -54,19 +69,31 @@ public class DeckManager : MonoBehaviour
     /// <param name="drawAmount"></param>
     public void DeckDraw(int drawAmount)
     {
-        StartCoroutine(DeckToHandDraw(drawAmount));
-        
+        EnqueueMethod(()=> DrawCoroutine(drawAmount));
     }
 
-   
-    private IEnumerator DeckToHandDraw(int drawAmount) //try and tweak later to make it feel better
+    public void HandCardToDiscard(GameObject card)
     {
+        EnqueueMethod(() => DiscardCardFromHandCoroutine(card));
+    }
+   
+    private IEnumerator DrawCoroutine(int drawAmount) 
+    {
+
+        if (_hand.Count >= 7 || //Hand is full we cannot draw anymore
+            (_deck.Count==0 && _discard.Count==0)) //If no cards in deck or discard can't draw
+        { 
+            yield return null; 
+        } 
+
+        if(_deck.Count==0 && _discard.Count > 0) //If deck is empty but have cards in discard
+        {
+            ShuffleList(_discard); //Shuffle the discard
+            AddToDeck(_deck, _discard); //Add the cards in the discard to the deck
+        }
+
         for (int i = drawAmount; i>0 ; i--)
         {
-            //
-            //ADD IF HAVE HAND SPACE AND CAN"T DRAW AND THERE ARE CARDS IN DISCARD THEN SHUFFLE
-            //
-            if (_hand.Count >= 7) { yield return null; } //Hand is full we cannot draw anymore
 
             //Get and remove card from deck list add to hand list
             AbstractCard cardDrawn = _deck[0];
@@ -84,37 +111,37 @@ public class DeckManager : MonoBehaviour
             _cardSlotsFilled[slotIndex] = true;
             _cardPrefabsInHand[cardDisplay] = slotIndex;
 
-            cardDisplay.transform.DOMove(cardSlot.position, _duration).SetEase(_ease);
+            cardDisplay.transform.DOMove(cardSlot.position, _tweenDuration).SetEase(_ease);
             
             //Adds a delay if more cards to draw
-            if (i > 1) { yield return new WaitForSeconds(_drawDelay); }
+            if (i > 1) { yield return new WaitForSeconds(_actionsDelay); }
             
         }
 
         yield return null;
     }
 
-     // BUG if try to discard while cards are drawin things get messed up
-    public void HandCardToDiscard(GameObject card)
+    private IEnumerator DiscardCardFromHandCoroutine(GameObject card)
     {
         //Find what slot card was in free that slot
         int indexSlot = _cardPrefabsInHand[card];
-        _cardSlotsFilled[indexSlot] = false;
+        _cardSlotsFilled[indexSlot] = false; //free the bool slot array
         Transform cardSlot = _cardSlots[indexSlot];
         
         //Update cards in hand and cards in discard
         AbstractCard abstractCard = card.GetComponent<CardDisplay>().GetCard();
         _hand.Remove(abstractCard);
         _discard.Add(abstractCard);
+        _cardPrefabsInHand.Remove(card);
 
         //Tween card to the discard deck position
-        card.transform.DOMove(_discardTransform.position, _duration).SetEase(_ease).OnComplete(() =>
+        card.transform.DOMove(_discardTransform.position, _tweenDuration).SetEase(_ease).OnComplete(() =>
         {
             Destroy(card);
         });
 
+        yield return new WaitForSeconds(_actionsDelay);
     }
-
 
     /// <summary>
     /// Chose one deck to add cards to from another place
@@ -129,20 +156,21 @@ public class DeckManager : MonoBehaviour
 
     public void AddToDeck(List<AbstractCard> deck, AbstractCard card)
     {
+        
         deck.Add(card);
     }
 
     /// <summary>
     /// Fisher-Yates algorithm to shuffle the deck.
     /// </summary>
-    public void ShuffleDeck()
+    public void ShuffleList(List<AbstractCard> deck)
     {
-        int n = _deck.Count;
+        int n = deck.Count;
         while (n > 1)
         {
             n--;
-            int k = Random.Range(0, n + 1);
-            (_deck[n], _deck[k]) = (_deck[k], _deck[n]);
+            int k = UnityEngine.Random.Range(0, n + 1);
+            (deck[n], deck[k]) = (deck[k], deck[n]);
         }
     }
 
@@ -178,5 +206,40 @@ public class DeckManager : MonoBehaviour
 
         Debug.Log("Couldn't find open leftmost slot called and all slots full.");
         return -1;
+    }
+    
+    
+    /// <summary>
+    /// Used to do coroutines in a list sequentially
+    /// </summary>
+    /// <param name="method">EnqueueMethod(()=> yourmethodhere(param))</param>
+    private void EnqueueMethod(Func<IEnumerator> method)
+    {
+        methodQueue.Enqueue(method);
+
+        if (!isQueueRunning) { StartCoroutine(ProcessQueue()); }
+    }
+
+    // Coroutine to process the method queue
+    private IEnumerator ProcessQueue()
+    {
+        isQueueRunning = true;
+
+        while (methodQueue.Count > 0)
+        {
+            Func<IEnumerator> method = methodQueue.Dequeue();
+            yield return StartCoroutine(method());
+        }
+
+        isQueueRunning = false;
+    }
+
+    private void UpdateDeckNum()
+    {
+        _deckNumTMP.text = _deck.Count.ToString();
+    }
+    private void UpdateDiscardNum()
+    {
+        _discardNumTMP.text = _discard.Count.ToString();
     }
 }
