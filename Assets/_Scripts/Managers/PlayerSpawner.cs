3ac2c14e6c23352bcbbb61b57d46ab77fe24fa4f
Tiago <102840379+YAGOTAGO@users.noreply.github.com>
2023-09-01 19:54:34 -0400

    In progress player spawner

--- Assets/_Scripts/Managers/PlayerSpawner.cs
+++ Assets/_Scripts/Managers/PlayerSpawner.cs
@@ -9,8 +9,12 @@ public class PlayerSpawner : NetworkBehaviour
 {
 
     [SerializeField] private GameObject _playerPrefab;
-    [SerializeField] private Scene _sceneToSpawnCharactersIn; //the scene which we want the characters to spawn in
+    [SerializeField] private string _sceneName; //the scene which we want the characters to spawn in
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
@@ -20,17 +24,50 @@ public class PlayerSpawner : NetworkBehaviour
     private void SpawnCharacters(string sceneName, LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut)
     {
 
-        Debug.Log("Spawn characters called " + OwnerClientId);
-        if(sceneName == _sceneToSpawnCharactersIn.name)
+        if(sceneName == _sceneName)
         {
-            foreach(ulong id in clientsCompleted)
+            if (IsServer)
             {
-                
+                Debug.Log("Server spawning characters " + OwnerClientId);
 
+                GameObject character1 = Instantiate(_playerPrefab);
+                GameObject character2 = Instantiate(_playerPrefab);
+                GameObject character3 = Instantiate(_playerPrefab);
+
+                character1.GetComponent<Character>().PutOnHexNode(GridManager.Instance.GridCoordTiles[new Vector3Int(-6, -17)], true);
+                character2.GetComponent<Character>().PutOnHexNode(GridManager.Instance.GridCoordTiles[new Vector3Int(-6, -5)], true);
+                character3.GetComponent<Character>().PutOnHexNode(GridManager.Instance.GridCoordTiles[new Vector3Int(-6, 7)], true);
+
+                character1.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
+                character2.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
+                character3.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
+
+            }
+            else
+            {
+                Debug.Log("Client spawning characters " + OwnerClientId);
+                SpawnClientCharactersServerRPC(OwnerClientId);
             }
 
         }
 
     }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SpawnClientCharactersServerRPC(ulong clientID)
+    {
+        GameObject character1 = Instantiate(_playerPrefab);
+        GameObject character2 = Instantiate(_playerPrefab);
+        GameObject character3 = Instantiate(_playerPrefab);
+
+        character1.GetComponent<Character>().PutOnHexNode(GridManager.Instance.GridCoordTiles[new Vector3Int(12, -17)], true);
+        character2.GetComponent<Character>().PutOnHexNode(GridManager.Instance.GridCoordTiles[new Vector3Int(13, -4)], true);
+        character3.GetComponent<Character>().PutOnHexNode(GridManager.Instance.GridCoordTiles[new Vector3Int(12, 9)], true);
+
+        character1.GetComponent<NetworkObject>().SpawnWithOwnership(clientID);
+        character2.GetComponent<NetworkObject>().SpawnWithOwnership(clientID);
+        character3.GetComponent<NetworkObject>().SpawnWithOwnership(clientID);
+    }
+
 
 }
