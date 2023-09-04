05240ef2d9350673a6d1abf2b3729bb1322f5e46
Tiago <102840379+YAGOTAGO@users.noreply.github.com>
2023-09-04 18:50:00 -0400

    Randomize who goes first

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -3,27 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : NetworkBehaviour
 {
     public static GameManager Instance;
+    public NetworkVariable<bool> IsServersTurn = new();
+
+    [SerializeField] private Button _endTurnButton;
 
     private void Awake()
     {
         Instance = this;
-        DontDestroyOnLoad(this);
-
+        _endTurnButton.onClick.AddListener(OnEndTurnButtonClick);
     }
 
-    private void Start()
+    public override void OnNetworkSpawn()
     {
-        
-        
-        //GameObject character1 = Instantiate(_characterPrefabToSpawn);
-        //character1.GetComponent<Character>().PutOnHexNode(GridManager.Instance.GridCoordTiles[new Vector3Int(0, 0)], true);
-        
+        if (IsServer)
+        {
+            IsServersTurn.Value = UnityEngine.Random.Range(0f, 1f) < 0.5f; //randomize who goes first
+        }
     }
+    private void OnEndTurnButtonClick()
+    {
+        if (IsServer && IsServersTurn.Value) //Server and is your turn
+        {
+            IsServersTurn.Value = false;
+        }
+        else if(!IsServer && !IsServersTurn.Value)
+        {
+            SwapTurnServerRPC();
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SwapTurnServerRPC()
+    {
+        IsServersTurn.Value = true;
+    }
+
     private void Update()
     {
         
