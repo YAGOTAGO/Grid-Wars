49632438a3be54913db82f4a82b7067c3fbd72f9
Tiago <102840379+YAGOTAGO@users.noreply.github.com>
2023-09-06 16:14:30 -0400

    Damage log sync

--- Assets/_Scripts/Managers/LogManager.cs
+++ Assets/_Scripts/Managers/LogManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -18,8 +19,30 @@ public class LogManager : NetworkBehaviour
     public void LogDamageAbility(string abilityName, DamageInfo dmgInfo, int damage)
     {
         string name = abilityName.Replace("Ability", "");
-        if(dmgInfo.Target == null) { return; }
-        _logTMP.text += $"\nCharacter #{dmgInfo.Source.CharacterID.Value} dealt <color=red>{damage}</color> damage to character #{dmgInfo.Target.CharacterID.Value} using {name}.";
+        if (dmgInfo.Target == null) { return; }
+        
+        FixedString128Bytes log = $"\nCharacter #{dmgInfo.Source.CharacterID.Value} dealt <color=red>{damage} damage</color> to character #{dmgInfo.Target.CharacterID.Value} using {name}.";
+
+        if (IsServer) //update all clients (includes server)
+        {
+            UpdateLogManagerClientRPC(log);
+        }
+        else //Send rpc that will then update all clients
+        {
+            UpdateLogManagerServerRPC(log);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void UpdateLogManagerServerRPC(FixedString128Bytes logString)
+    {
+        UpdateLogManagerClientRPC(logString);
+    }
+
+    [ClientRpc]
+    public void UpdateLogManagerClientRPC(FixedString128Bytes logString)
+    {
+        _logTMP.text += logString;
     }
 
 }
